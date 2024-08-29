4f218f310bed023d69c79dcc09480e1f70fdf0e8
ViniTvll <vinitavella18@gmail.com>
2024-08-28 19:26:07 -0500

    Primer commit

---
+++ src/main/java/com/example/lab2_20182693/Lab220182693Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20182693;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220182693Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab220182693Application.class, args);
+	}
+
+}
